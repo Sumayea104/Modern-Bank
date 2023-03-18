afff921cf158dccd647841dbccd746af7fa4856a
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-18 12:54:42 +0600

    calculate

---
+++ js/calculation.js
@@ -0,0 +1,5 @@
+// console.log('clicked')
+
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    // console.log('btn clicked');
+})
--- js/withdraw.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('btn-withdraw').addEventListener('click',function(){
-    // console.log('clicked')
-    const withdrawField = document.getElementById('withdraw-input');
-    const newWithdrawAmountString = withdrawField.value;
-
-    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
-    console.log(newWithdrawAmount);
-})
